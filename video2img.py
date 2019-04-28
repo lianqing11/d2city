663e0c3f301138d1bd75266bdf22e600d51fc6ab
lianqing11 <lianqinglalala@gmail.com>
2019-04-28 19:42:03 +0800

    update

--- video2img.py
+++ video2img.py
@@ -26,7 +26,7 @@ def convert(i):
     vid = imageio.read(i)
     for num, img in enumerate(vid.iter_data()):
         save_dir = i.split(".mp4")[0]
-        save_dir = save_dir+ "_{}.png".format(num)
+        save_dir = save_dir+ "_{}.jpg".format(num)
         img = Image.fromarray(img)
         img.save(save_dir)
 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     video_list = glob.glob(args.dir + "000*/*.mp4")
     print("transfer video num {}".format(len(video_list)))
 
-    p = multiprocessing.Pool(24)
+    p = multiprocessing.Pool(48)
     b = p.map(convert, video_list)
     print("final transformation")
 
